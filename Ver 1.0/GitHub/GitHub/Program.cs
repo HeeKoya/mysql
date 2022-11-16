e7454972857a5cc7b3603220caaf26b9f5ec071a
HeeKoya <117077858+HeeKoya@users.noreply.github.com>
2022-11-16 08:49:37 +0300

    Ver1.0

---
+++ Ver 1.0/GitHub/GitHub/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            Console.WriteLine("SOME WORDS");
+
+            if (a == true)
+            {
+                Console.WriteLine("ACCESS");
+            }
+            else
+            {
+                Console.WriteLine("DENIED");
+            }
+            
+
+
+        }
+    }
+}
